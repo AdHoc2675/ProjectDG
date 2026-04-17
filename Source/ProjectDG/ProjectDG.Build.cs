0f65ee6272ed357d6e27e5ec5b228cc9a0db2b31
Sam <rndtn47@gmail.com>
2026-04-17 16:26:31 +0900

    change folder 2

--- Source/ProjectDG/ProjectDG.Build.cs
+++ Source/ProjectDG/ProjectDG.Build.cs
@@ -27,20 +27,7 @@ public class ProjectDG : ModuleRules
 		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks" });
 
 		PublicIncludePaths.AddRange(new string[] {
-			"ProjectDG",
-			"ProjectDG/Variant_Platforming",
-			"ProjectDG/Variant_Platforming/Animation",
-			"ProjectDG/Variant_Combat",
-			"ProjectDG/Variant_Combat/AI",
-			"ProjectDG/Variant_Combat/Animation",
-			"ProjectDG/Variant_Combat/Gameplay",
-			"ProjectDG/Variant_Combat/Interfaces",
-			"ProjectDG/Variant_Combat/UI",
-			"ProjectDG/Variant_SideScrolling",
-			"ProjectDG/Variant_SideScrolling/AI",
-			"ProjectDG/Variant_SideScrolling/Gameplay",
-			"ProjectDG/Variant_SideScrolling/Interfaces",
-			"ProjectDG/Variant_SideScrolling/UI"
+			"ProjectDG"
 		});
 
 		// Uncomment if you are using Slate UI
