fa3569d98532f67d1c14b7c451aee68cadd1b108
Minuck Choi <54134687+AdHoc2675@users.noreply.github.com>
2026-04-20 13:55:47 +0900

    sample 업데이트

---
+++ Source/ProjectDG/Public/Character/Player/WarriorCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/Player/PlayerCharacterBase.h"
+#include "WarriorCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTDG_API AWarriorCharacter : public APlayerCharacterBase
+{
+	GENERATED_BODY()
+	
+};
