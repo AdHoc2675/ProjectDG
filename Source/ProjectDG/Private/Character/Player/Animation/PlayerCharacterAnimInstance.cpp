7462b2782b10e3b7f724bba40a015568343b5041
SeongAA-Park <seongah0201@gmail.com>
2026-04-29 21:15:11 +0900

    Feat: 플레이어 캐릭터의 기본 이동 애니메이션 시스템 및 로직 구현
    - C++ PlayerCharacterAnimInstance 클래스 구현을 통한 캐릭터 데이터와 ABP 연동
    - ABP_PlayerCharacter 내 스테이트 머신, State Alias, 가속도 기반 전환 로직 구축
    - GroundSpeed와 Acceleration 변수를 활용한 이동 상태 동기화 정밀화

---
+++ Source/ProjectDG/Private/Character/Player/Animation/PlayerCharacterAnimInstance.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/Player/Animation/PlayerCharacterAnimInstance.h"
+#include "Character/Player/PlayerCharacterBase.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UPlayerCharacterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	
+	// 소유한 폰을 PlayerCharacterBase로 캐스팅하여 저장
+	PlayerCharacter = Cast<APlayerCharacterBase>(TryGetPawnOwner());
+	if (PlayerCharacter)
+	{
+		PlayerMovement = PlayerCharacter->GetCharacterMovement();
+	}
+}
+
+void UPlayerCharacterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+	
+	// 캐릭터 참조가 유효하지 않으면 재시도
+	if (!PlayerCharacter)
+	{
+		PlayerCharacter = Cast<APlayerCharacterBase>(TryGetPawnOwner());
+		if (PlayerCharacter)
+		{
+			PlayerMovement = PlayerCharacter->GetCharacterMovement();
+		}
+	}
+
+	if (!PlayerCharacter || !PlayerMovement) return;
+
+	// 이동 속도 업데이트 (Z축 제외 수평 속도)
+	GroundSpeed = PlayerCharacter->GetVelocity().Size2D();
+	
+	// 가속 여부 업데이트
+	bIsAccelerating = PlayerMovement->GetCurrentAcceleration().Size() > 0.f;
+
+	// 공중 상태 업데이트
+	bIsFalling = PlayerMovement->IsFalling();
+
+	// 캐릭터 상태 변수 동기화
+	// PlayerCharacterBase에 추가한 Getter를 통해 값을 가져옵니다.
+	bIsSprinting = PlayerCharacter->IsSprinting();
+	bIsDodging = PlayerCharacter->IsDodging();
+	bIsParkouring = PlayerCharacter->IsParkouring();
+	
+}
---
+++ Source/ProjectDG/Public/Character/Player/Animation/PlayerCharacterAnimInstance.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PlayerCharacterAnimInstance.generated.h"
+
+/**
+ * UPlayerCharacterAnimInstance
+ *
+ * PlayerCharacterBase의 상태를 받아 ABP(Animation Blueprint)로 전달하는 클래스입니다.
+ * PlayerCharacterMovementData의 명명 규칙을 따릅니다.
+ */
+
+UCLASS()
+class PROJECTDG_API UPlayerCharacterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	// 초기화 시 호출
+	virtual void NativeInitializeAnimation() override;
+
+	// 매 프레임 업데이트 시 호출
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	/** 참조 객체 */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character")
+	class APlayerCharacterBase* PlayerCharacter;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Character")
+	class UCharacterMovementComponent* PlayerMovement;
+
+	/** ABP에서 사용할 상태 변수들 */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	float GroundSpeed;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	bool bIsAccelerating;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	bool bIsFalling;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	bool bIsSprinting;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	bool bIsDodging;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+	bool bIsParkouring;
+	
+};
