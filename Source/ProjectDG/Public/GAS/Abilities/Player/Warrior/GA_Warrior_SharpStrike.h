3a4182de60ce8d05e46068a5547a13cd8b435709
SeongAA-Park <seongah0201@gmail.com>
2026-05-12 09:19:01 +0900

    Feat: Warrior SharpStrike 콤보 몽타주 연동
     - GA_Warrior_SharpStrike에서 SharpStrike 몽타주를 Combo_1부터 재생
     - ComboInputWindow Open/Close와 ComboBranch GameplayEvent 수신 처리 추가
     - 스킬 키 Hold 상태를 기반으로 Combo_1 -> Combo_2 -> Combo_3 섹션 점프 처리
     - BP_GA_Warrior_SharpStrike에 SharpStrike 몽타주 참조 설정
     - AM_GF_Warrior_SharpStrike에 콤보 섹션과 입력/분기 Notify 구성

--- Source/ProjectDG/Public/GAS/Abilities/Player/Warrior/GA_Warrior_SharpStrike.h
+++ Source/ProjectDG/Public/GAS/Abilities/Player/Warrior/GA_Warrior_SharpStrike.h
@@ -20,4 +20,64 @@ public:
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* OwnerInfo, 
 		const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, 
+		const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+	
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "SharpStrike|Animation")
+	TObjectPtr<UAnimMontage> SharpStrikeMontage;
+
+	UPROPERTY(EditDefaultsOnly, Category = "SharpStrike|Combo")
+	FName Combo1SectionName = TEXT("Combo_1");
+
+	UPROPERTY(EditDefaultsOnly, Category = "SharpStrike|Combo")
+	FName Combo2SectionName = TEXT("Combo_2");
+
+	UPROPERTY(EditDefaultsOnly, Category = "SharpStrike|Combo")
+	FName Combo3SectionName = TEXT("Combo_3");
+
+private:
+	UPROPERTY()
+	TObjectPtr<class UAbilityTask_PlayMontageAndWait> MontageTask;
+
+	UPROPERTY()
+	TObjectPtr<class UAbilityTask_WaitGameplayEvent> ComboInputWindowOpenTask;
+
+	UPROPERTY()
+	TObjectPtr<class UAbilityTask_WaitGameplayEvent> ComboInputWindowCloseTask;
+
+	UPROPERTY()
+	TObjectPtr<class UAbilityTask_WaitGameplayEvent> ComboBranchTask;
+
+	int32 CurrentComboIndex = 1;
+
+	bool bComboInputWindowOpen = false;
+	bool bComboInputBuffered = false;
+
+	void ResetComboState();
+
+	bool IsSharpStrikeInputHeld() const;
+	void TryBufferComboInputFromHeldState();
+	void TryJumpToNextComboSection();
+
+	UFUNCTION()
+	void OnComboInputWindowOpened(FGameplayEventData Payload);
+
+	UFUNCTION()
+	void OnComboInputWindowClosed(FGameplayEventData Payload);
+
+	UFUNCTION()
+	void OnComboBranch(FGameplayEventData Payload);
+
+	UFUNCTION()
+	void OnMontageCompleted();
+
+	UFUNCTION()
+	void OnMontageInterrupted();
+
+	UFUNCTION()
+	void OnMontageBlendOut();
+
+	UFUNCTION()
+	void OnMontageCancelled();
 };
