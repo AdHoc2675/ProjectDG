93e8c9ad696b30c10ef1a80ea62c84b04324d6c4
Minuck Choi <54134687+AdHoc2675@users.noreply.github.com>
2026-05-12 13:54:53 +0900

    UDGEnemyStatusWidget 클래스 추가 및 기본 구조 구현
    - UDGEnemyStatusWidget 헤더(.h) 및 소스(.cpp) 파일 추가.
    - 블루프린트에서 호출 가능한(BlueprintCallable) Public API 선언 (InitEnemyStatus, UpdateHealth, UpdateGroggyGauge, SetEnemyName, ShowEnemyStatus, HideEnemyStatus).
    - 현재 소스(.cpp) 파일은 기본 뼈대(Stub)만 구현된 상태임.
    - 주요 UI 위젯 바인딩 (EnemyNameText, HealthPercentText, HealthLayerCountText, HealthProgressBar, GroggyProgressBar).
    - 일정 시간 후 UI를 숨기는 타이머(Hide Timer) 및 최대 체력 줄 수(MaxHealthBars) 설정 추가.
    - WBP_DGEnemyStatusWidget.uasset 블루프린트 에셋 신규 추가.

---
+++ Source/ProjectDG/Private/UI/Widget/Enemy/DGEnemyStatusWidget.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Widget/Enemy/DGEnemyStatusWidget.h"
+
+
+void UDGEnemyStatusWidget::UpdateHealth(float CurrentHealth, float MaxHealth) {
+
+
+}
+
+void UDGEnemyStatusWidget::UpdateGroggyGauge(float CurrentGroggy, float MaxGroggy) {
+
+}
+
+void UDGEnemyStatusWidget::SetEnemyName(const FString& Name) {
+
+}
+
+void UDGEnemyStatusWidget::ShowEnemyStatus() {
+
+}
+
+void UDGEnemyStatusWidget::HideEnemyStatus() {
+
+}
