e8a254ddf2a9718baea4be72886ccce14f8630dc
SeongAA-Park <seongah0201@gmail.com>
2026-05-21 17:03:11 +0900

    Feat: Warrior DoomStrike GA 추가
      - DoomStrike 입력 및 돌진 시작 처리를 위한 GameplayTag 추가
      - GA_Warrior_DoomStrike 클래스를 추가해 몽타주 재생 및 타겟 검증 흐름 구현
      - ANS 이벤트 수신 시 RootMotionMoveToForce 기반 돌진 이동을 실행하도록 구성
      - 공격 히트 이벤트 수신 시 서버 권위로 DoomStrike 데미지를 적용하도록 처리

--- Source/ProjectDG/Public/Core/DG_GameplayTags.cpp
+++ Source/ProjectDG/Public/Core/DG_GameplayTags.cpp
@@ -31,9 +31,14 @@ namespace DGGameplayTags
 	
 	UE_DEFINE_GAMEPLAY_TAG(Event_Attack_HitWindow_Begin, "Event.Attack.HitWindow.Begin")
 	
+	// DoomStrike(파멸의 맹타) 몽타주 출력 시 타겟팅 대상에게 Dash 시작을 알리는 ANS_SendGameplayEventWindow에서 사용될 태그
+	UE_DEFINE_GAMEPLAY_TAG(Event_Movement_Warrior_DoomStrike_DashBegin, "Event.Movement.Warrior.DoomStrike.DashBegin")
+	
+	
 	// 전사스킬 예리한일격 탭으로 전달 시 필요 태그
 	UE_DEFINE_GAMEPLAY_TAG(Event_Input_Warrior_SharpStrike, "Event.Input.Warrior.SharpStrike")
 	UE_DEFINE_GAMEPLAY_TAG(Event_Input_Warrior_LeapingSlam, "Event.Input.Warrior.LeapingSlam")
+	UE_DEFINE_GAMEPLAY_TAG(Event_Input_Warrior_DoomStrike, "Event.Input.Warrior.DoomStrike")
 	
 	
 	// Skill.Common
