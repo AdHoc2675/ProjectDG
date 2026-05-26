a38ed9756702fb1f8c4f134463d243380877dc4e
SeongAA-Park <seongah0201@gmail.com>
2026-05-27 08:13:25 +0900

    Feat: PlayerSkillBase 쿨다운 적용 로직 추가
      - GE_SkillCoolDown을 공통 스킬 쿨다운 GameplayEffect로 추가
      - PlayerSkillBase에서 CooldownGameplayEffectClass를 코드 레벨로 설정
      - PlayerSkillData의 Cooldown 값을 Data.Cooldown SetByCaller로 전달
      - PlayerSkillData의 CooldownTag를 쿨다운 검사 및 적용 태그로 사용
      - CommitAbility 흐름에서 스킬별 쿨다운이 적용되도록 공통 처리 추가

---
+++ Source/ProjectDG/Public/GAS/Effects/Skills/GE_SkillCoolDown.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "GE_SkillCoolDown.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTDG_API UGE_SkillCoolDown : public UGameplayEffect
+{
+	GENERATED_BODY()
+	
+public:
+	UGE_SkillCoolDown();
+	
+};
