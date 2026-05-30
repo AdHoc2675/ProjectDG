fed197c3daf889652e44ffa2e469c5a2e3e97468
SeongAA-Park <seongah0201@gmail.com>
2026-05-30 20:38:56 +0900

    Feat: Assassin ShadowAssault GA 추가
      - 암살자 암습 스킬용 GA_Assassin_ShadowAssault 클래스 추가
      - ShadowAssault 전용 이동 이벤트 기반 타겟 후방 이동 처리 구현
      - BP_GA_ShadowAssault 생성 및 스킬 데이터에서 사용할 AbilityClass 준비

---
+++ Source/ProjectDG/Private/GAS/Abilities/Player/Assassin/GA_Assassin_ShadowAssault.cpp
@@ -0,0 +1,242 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GAS/Abilities/Player/Assassin/GA_Assassin_ShadowAssault.h"
+
+#include "Abilities/Tasks/AbilityTask_ApplyRootMotionMoveToForce.h"
+#include "Abilities/Tasks/AbilityTask_WaitGameplayEvent.h"
+#include "Components/CapsuleComponent.h"
+#include "Core/DG_GameplayTags.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+UGA_Assassin_ShadowAssault::UGA_Assassin_ShadowAssault()
+{
+      InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+      NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::LocalPredicted;
+
+      AbilityTags.AddTag(DGGameplayTags::Skill_Assassin_ShadowAssault);
+      ActivationOwnedTags.AddTag(DGGameplayTags::State_Skill_Assassin_ShadowAssault_Active);
+}
+
+void UGA_Assassin_ShadowAssault::EndAbility(
+              const FGameplayAbilitySpecHandle Handle,
+              const FGameplayAbilityActorInfo* ActorInfo,
+              const FGameplayAbilityActivationInfo ActivationInfo,
+              bool bReplicateEndAbility,
+              bool bWasCancelled
+)
+{
+      ClearIgnoredDashTargetCollision();
+
+      MoveBeginTask = nullptr;
+      MoveBehindTargetTask = nullptr;
+
+      Super::EndAbility(
+                      Handle,
+                      ActorInfo,
+                      ActivationInfo,
+                      bReplicateEndAbility,
+                      bWasCancelled
+      );
+}
+
+void UGA_Assassin_ShadowAssault::ResetTargetMontageState()
+{
+      ClearIgnoredDashTargetCollision();
+
+      Super::ResetTargetMontageState();
+
+      MoveBeginTask = nullptr;
+      MoveBehindTargetTask = nullptr;
+}
+
+void UGA_Assassin_ShadowAssault::StartTargetMontageEventTasks()
+{
+      Super::StartTargetMontageEventTasks();
+
+      MoveBeginTask = UAbilityTask_WaitGameplayEvent::WaitGameplayEvent(
+                      this,
+                      DGGameplayTags::Event_Movement_Assassin_ShadowAssault_MoveBegin.GetTag(),
+                      nullptr,
+                      false,
+                      true
+      );
+
+      if (MoveBeginTask)
+      {
+              MoveBeginTask->EventReceived.AddDynamic(
+                              this,
+                              &UGA_Assassin_ShadowAssault::OnMoveBegin
+              );
+              MoveBeginTask->ReadyForActivation();
+      }
+}
+
+void UGA_Assassin_ShadowAssault::ExecuteTargetSkill(AActor* TargetActor, const FGameplayEventData& Payload)
+{
+      if (!HasAuthorityAvatar())
+      {
+              return;
+      }
+
+      if (!TargetActor)
+      {
+              return;
+      }
+
+      ApplyDamageToTarget(
+                      TargetActor,
+                      0.f,
+                      GetSkillDamageMultiplier(),
+                      GetSkillTag(),
+                      TargetActor->GetActorLocation(),
+                      true,
+                      GetSkillGroggyDamage()
+      );
+
+      ApplyStatusEffectToTarget(TargetActor);
+}
+
+void UGA_Assassin_ShadowAssault::OnMoveBegin(FGameplayEventData Payload)
+{
+      if (!IsCurrentTargetStillValid())
+      {
+              EndTargetMontageAbility(true);
+              return;
+      }
+
+      FaceCurrentTarget();
+
+      const float MoveDuration = FMath::Max(Payload.EventMagnitude, 0.01f);
+      StartMoveBehindTarget(MoveDuration);
+}
+
+void UGA_Assassin_ShadowAssault::StartMoveBehindTarget(float Duration)
+{
+      AActor* TargetActor = CurrentTargetResult.TargetActor;
+      if (!TargetActor)
+      {
+              EndTargetMontageAbility(true);
+              return;
+      }
+
+      FVector BehindTargetLocation;
+      if (!BuildBehindTargetLocation(TargetActor, BehindTargetLocation))
+      {
+              EndTargetMontageAbility(true);
+              return;
+      }
+
+      IgnoreDashTargetCollision(TargetActor);
+
+      MoveBehindTargetTask = UAbilityTask_ApplyRootMotionMoveToForce::ApplyRootMotionMoveToForce(
+                      this,
+                      TEXT("ShadowAssaultMoveBehindTarget"),
+                      BehindTargetLocation,
+                      Duration,
+                      false,
+                      MOVE_Walking,
+                      false,
+                      nullptr,
+                      ERootMotionFinishVelocityMode::SetVelocity,
+                      FVector::ZeroVector,
+                      0.f
+      );
+
+      if (MoveBehindTargetTask)
+      {
+              MoveBehindTargetTask->OnTimedOut.AddDynamic(this, &UGA_Assassin_ShadowAssault::OnMoveFinished);
+              MoveBehindTargetTask->OnTimedOutAndDestinationReached.AddDynamic(this, &UGA_Assassin_ShadowAssault::OnMoveFinished);
+              MoveBehindTargetTask->ReadyForActivation();
+      }
+}
+
+bool UGA_Assassin_ShadowAssault::BuildBehindTargetLocation(AActor* TargetActor, FVector& OutLocation) const
+{
+      AActor* AvatarActor = GetAvatarActorFromAbility();
+      if (!AvatarActor || !TargetActor)
+      {
+              return false;
+      }
+
+      const FVector AvatarLocation = AvatarActor->GetActorLocation();
+      const FVector TargetLocation = TargetActor->GetActorLocation();
+
+      FVector TargetBackward = -TargetActor->GetActorForwardVector();
+      TargetBackward.Z = 0.f;
+
+      if (!TargetBackward.Normalize())
+      {
+              return false;
+      }
+
+      OutLocation = TargetLocation + TargetBackward * BehindTargetDistance;
+      OutLocation.Z = AvatarLocation.Z;
+
+      return true;
+}
+
+void UGA_Assassin_ShadowAssault::IgnoreDashTargetCollision(AActor* TargetActor)
+{
+      ACharacter* AvatarCharacter = Cast<ACharacter>(GetAvatarActorFromAbility());
+      if (!AvatarCharacter || !TargetActor)
+      {
+              return;
+      }
+
+      UCapsuleComponent* Capsule = AvatarCharacter->GetCapsuleComponent();
+      if (!Capsule)
+      {
+              return;
+      }
+
+      Capsule->IgnoreActorWhenMoving(TargetActor, true);
+      IgnoredDashTarget = TargetActor;
+}
+
+void UGA_Assassin_ShadowAssault::ClearIgnoredDashTargetCollision()
+{
+      ACharacter* AvatarCharacter = Cast<ACharacter>(GetAvatarActorFromAbility());
+      if (!AvatarCharacter || !IgnoredDashTarget)
+      {
+              IgnoredDashTarget = nullptr;
+              return;
+      }
+
+      UCapsuleComponent* Capsule = AvatarCharacter->GetCapsuleComponent();
+      if (Capsule)
+      {
+              Capsule->IgnoreActorWhenMoving(IgnoredDashTarget, false);
+      }
+
+      IgnoredDashTarget = nullptr;
+}
+
+void UGA_Assassin_ShadowAssault::FaceTargetFromCurrentLocation()
+{
+      AActor* AvatarActor = GetAvatarActorFromAbility();
+      AActor* TargetActor = CurrentTargetResult.TargetActor;
+
+      if (!AvatarActor || !TargetActor)
+      {
+              return;
+      }
+
+      FVector DirectionToTarget = TargetActor->GetActorLocation() - AvatarActor->GetActorLocation();
+      DirectionToTarget.Z = 0.f;
+
+      if (DirectionToTarget.Normalize())
+      {
+              AvatarActor->SetActorRotation(DirectionToTarget.Rotation());
+      }
+}
+
+void UGA_Assassin_ShadowAssault::OnMoveFinished()
+{
+      ClearIgnoredDashTargetCollision();
+      FaceTargetFromCurrentLocation();
+}
+
+
+
+
---
+++ Source/ProjectDG/Public/GAS/Abilities/Player/Assassin/GA_Assassin_ShadowAssault.h
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GAS/Abilities/Base/GA_TargetMontageSkillBase.h"
+#include "GA_Assassin_ShadowAssault.generated.h"
+
+class UAbilityTask_ApplyRootMotionMoveToForce;
+class UAbilityTask_WaitGameplayEvent;
+
+/**
+ * 암살자 3번 스킬: 암습
+ *
+ * 타겟 획득 / Commit / 몽타주 / Hit 이벤트는 UGA_TargetMontageSkillBase를 사용하고,
+ * 공격 후 타겟 반대 방향으로 이동하는 처리는 이 클래스에서 담당한다.
+ */
+UCLASS()
+class PROJECTDG_API UGA_Assassin_ShadowAssault : public UGA_TargetMontageSkillBase
+{
+	GENERATED_BODY()
+
+public:
+	UGA_Assassin_ShadowAssault();
+
+	virtual void EndAbility(
+					const FGameplayAbilitySpecHandle Handle,
+					const FGameplayAbilityActorInfo* ActorInfo,
+					const FGameplayAbilityActivationInfo ActivationInfo,
+					bool bReplicateEndAbility,
+					bool bWasCancelled
+	) override;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "ShadowAssault|Movement")
+	float BehindTargetDistance = 160.f;
+
+	UPROPERTY()
+	TObjectPtr<UAbilityTask_WaitGameplayEvent> MoveBeginTask = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<UAbilityTask_ApplyRootMotionMoveToForce> MoveBehindTargetTask = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<AActor> IgnoredDashTarget = nullptr;
+
+protected:
+	virtual void ResetTargetMontageState() override;
+
+	virtual void StartTargetMontageEventTasks() override;
+
+	virtual void ExecuteTargetSkill(AActor* TargetActor, const FGameplayEventData& Payload) override;
+
+	bool BuildBehindTargetLocation(AActor* TargetActor, FVector& OutLocation) const;
+
+	void StartMoveBehindTarget(float Duration);
+
+	void IgnoreDashTargetCollision(AActor* TargetActor);
+
+	void ClearIgnoredDashTargetCollision();
+
+	void FaceTargetFromCurrentLocation();
+
+	UFUNCTION()
+	void OnMoveBegin(FGameplayEventData Payload);
+
+	UFUNCTION()
+	void OnMoveFinished();
+};
