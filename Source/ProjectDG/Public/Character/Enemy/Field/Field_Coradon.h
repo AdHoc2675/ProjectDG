5e9363179e12029f6da94d7379f9becba078858a
kim sa yun <kim106205304@gmail.com>
2026-06-03 22:46:13 +0900

    몬스터 8 종류 추가

---
+++ Source/ProjectDG/Public/Character/Enemy/Field/Field_Coradon.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/Enemy/Field/FieldEnemyBase.h"
+#include "Field_Coradon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTDG_API AField_Coradon : public AFieldEnemyBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/ProjectDG/Public/Character/Enemy/Field/Field_DracoWar3.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/Enemy/Field/FieldEnemyBase.h"
+#include "Field_DracoWar3.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTDG_API AField_DracoWar3 : public AFieldEnemyBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
