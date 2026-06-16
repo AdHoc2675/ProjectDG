8c3bc0189589dee4b4cc654a3d8e619eca130514
Minuck Choi <54134687+AdHoc2675@users.noreply.github.com>
2026-06-16 15:11:37 +0900

    레벨업 시 위젯 팝업

--- Source/ProjectDG/Private/UI/Widget/DGOverlayWidget.cpp
+++ Source/ProjectDG/Private/UI/Widget/DGOverlayWidget.cpp
@@ -1,8 +1,9 @@
-﻿#include "UI/Widget/DGOverlayWidget.h"
+#include "UI/Widget/DGOverlayWidget.h"
 #include "UI/Widget/DGPlayerStatWidget.h"
 #include "UI/Widget/Enemy/DGEnemyStatusWidget.h"
 #include "UI/Widget/DGMiniMapWidget.h"
 #include "UI/Widget/DGPartyListWidget.h"
+#include "UI/Widget/Toggleable/DGLevelUpAnnouncementWidget.h"
 
 #include "UI/WidgetController/DGOverlayWidgetController.h"
 
@@ -36,8 +37,12 @@ void UDGOverlayWidget::InitializeSubWidgets()
 			PartyListWidget->BindToController(OverlayController);
 		}
 
+		if (LevelUpAnnouncementWidget)
+		{
+			LevelUpAnnouncementWidget->BindToController(OverlayController);
+		}
+
 		// 2. 나중에 추가될 다른 자식 위젯들도 여기서 바인딩
-		// if (EnemyStatusWidget) { ... }
 		// if (ChatWidget) { ... }
 
 	}
