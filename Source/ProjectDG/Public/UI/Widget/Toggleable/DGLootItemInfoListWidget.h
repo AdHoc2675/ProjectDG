afc1b59118622ba7410c64e2be438275a03001a2
Minuck Choi <54134687+AdHoc2675@users.noreply.github.com>
2026-06-16 16:45:40 +0900

    습득 아이템 정보 위젯 껍데기 생성

---
+++ Source/ProjectDG/Public/UI/Widget/Toggleable/DGLootItemInfoListWidget.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Widget/DGUserWidget.h"
+#include "DGLootItemInfoListWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTDG_API UDGLootItemInfoListWidget : public UDGUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/ProjectDG/Public/UI/Widget/Toggleable/DGLootItemInfoWidget.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Widget/DGUserWidget.h"
+#include "DGLootItemInfoWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTDG_API UDGLootItemInfoWidget : public UDGUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
